2c84527092d5903f6fda8dfebd4b4f13c491ac74
Daniel Ludwig <ludwig.1997.1@gmx.de>
2025-03-31 22:56:30 +0200

    NDN-1 | updated dependencies

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2025 Daniel Ludwig")]
 
 // The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.0.0.2017")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "3.1.2.9001")]
 
 // The license your plugin code is using
 [assembly: AssemblyMetadata("License", "MPL-2.0")]
