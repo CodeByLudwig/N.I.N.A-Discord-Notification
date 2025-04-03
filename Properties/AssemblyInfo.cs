38fa7dcded8d16f5c1232a56e9dae65698c96d0e
Daniel Ludwig <35924377+CodeByLudwig@users.noreply.github.com>
2025-04-03 13:57:57 +0200

    Update github-action.yaml

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -39,10 +39,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("Homepage", "")]
 
 //[Optional] Common tags that quickly describe your plugin
-[assembly: AssemblyMetadata("Tags", "")]
+[assembly: AssemblyMetadata("Tags", "Discord")]
 
 //[Optional] A link that will show a log of all changes in between your plugin's versions
-[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/CodeByLudwig/N.I.N.A-Discord-Notification/CHANGELOG.md")]
+[assembly: AssemblyMetadata("ChangelogURL", "https://github.com/CodeByLudwig/N.I.N.A-Discord-Notification/blob/develop/CHANGELOG.md")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
 [assembly: AssemblyMetadata("FeaturedImageURL", "")]
